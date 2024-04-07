ec383615d380cefb54af0d14cdd7e1da8974e232
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-04-06 21:55:32 -0400

    presque fini!!! Je n'arrive pas a avoir mon token for some reason

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Middleware\EnsureUserIsAdmin;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::controller(ProduitController::class)->group(function() {
-    Route::get('/produit/{id}', 'show')->name('produitApi');
-    Route::post('/insertion/produit', 'store')->name('insertionProduitApi');
+    Route::get('/produit/{id}', 'show')
+        ->name('produitApi')
+        ->middleware('auth:sanctum');
+    Route::post('/insertion/produit', 'store')
+        ->name('insertionProduitApi')
+        ->middleware(['auth:sanctum', EnsureUserIsAdmin::class]);
 });
 
 Route::controller(CategorieController::class)->group(function() {
     Route::get('/categories', 'show')->name('categoriesApi');
-    Route::post('/insertion/categorie', 'store')->name('insertionCategorieApi');
+    Route::post('/insertion/categorie', 'store')
+    ->name('insertionCategorieApi')
+    ->middleware(['auth:sanctum', EnsureUserIsAdmin::class]);
 });
+
+Route::post('/token', [RegisteredUserController::class, 'show'])->name('token');
