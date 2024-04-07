b27532c6338801bafb87f92635cdd858676d0841
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-04-06 21:12:28 -0400

    partie 8 terminé, middleroute selon le niveau de droit de l'utilisateur

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Models\Categorie;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Middleware\EnsureUserIsAdmin;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,9 +26,15 @@ Route::controller(ProduitController::class)->group(function() {
     Route::get('/produits', 'index')->name('produits');
     Route::get('/produit/{id}', 'show')->name('produit');
     Route::get('/produits/categorie/{id}', 'show')->name('produitsCategorie');
-    Route::get('/modification/produit', 'edit')->name('modificationProduit');
-    Route::post('/suppression/produit', 'destroy')->name('suppressionProduit');
-    Route::post('/enregistrement/produit/{id}', 'update')->name('enregistrementProduit');
+    Route::get('/modification/produit', 'edit')
+        ->name('modificationProduit')
+        ->middleware(EnsureUserIsAdmin::class);
+    Route::post('/suppression/produit', 'destroy')
+        ->name('suppressionProduit')
+        ->middleware(EnsureUserIsAdmin::class);
+    Route::post('/enregistrement/produit/{id}', 'update')
+        ->name('enregistrementProduit')
+        ->middleware(EnsureUserIsAdmin::class);
 
 });
 
@@ -40,6 +47,19 @@ Route::controller(CommentaireController::class)->group(function() {
     Route::post('/insertionCommentaire', 'store')->name('insertionCommentaire');
     Route::get('/confirmationCommentaire', 'show')->name('confirmationCommentaire');
    
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Toutes les routes du contrôleur " CommentaireController" sont maintenant prisonnières du
+    // "middleware" relatif à l’authentification. Ce faisant, un utilisateur doit préalablement
+    // s’authentifier pour accéder à une des routes du contrôleur " CommentaireController".
+    Route::controller(CommentaireController::class)->group(function() {
+        Route::get('/redaction/commentaire', 'create')->name('redactionCommentaire');
+        Route::post('/insertion/commentaire', 'store')->name('insertionCommentaire');
+    });
 });
 
 
