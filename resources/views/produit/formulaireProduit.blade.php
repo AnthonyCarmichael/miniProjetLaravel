667878ea94ef689754b1f2aeb2dcf6c09335be62
Anthony Carmichael <anthonycarmichael0@gmail.com>
2024-04-23 09:23:09 -0400

    prelab langue

--- resources/views/produit/formulaireProduit.blade.php
+++ resources/views/produit/formulaireProduit.blade.php
@@ -15,20 +15,24 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 space-y-4">
                     <form method="post" action="{{ route('enregistrementProduit',['id' => $produit->id_produit]) }}" class="border-solid border-2 border-gray-500 rounded-xl w-4/5 m-auto" id="formProduit">
-                        @csrf 
-                    
+                        @csrf
+
                         <label for="produit" class="text-right">Produit:</label>
                         <input type="text" name="produit" id="produit" value="{{ $produit->produit }}">
-                    
+
 
                         <label for="description" class="text-right">Description:</label>
                         <input type="text" name="description" id="description" value="{{ $produit->description }}">
-                    
+
                         <label for="prix" class="text-right">Prix:</label>
                         <input type="text" name="prix" id="prix" value="{{ $produit->prix }}">
 
                         <div class="full-width flex border justify-self-end">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-end">Modifier</button>
+                            <button
+                                type="submit"
+                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Modifier
+                            </button>
                         </div>
                     </form>
                 </div>
