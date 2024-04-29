c1ce17c71d07817085bbb0142400673b83a2bd3b
AnthonyCarmichael <anthonycarmichael0@gmail.com>
2024-04-28 21:10:35 -0400

    oublier de save panier

--- resources/views/commande/panier.blade.php
+++ resources/views/commande/panier.blade.php
@@ -7,23 +7,38 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div id="panierGrid" class="p-6 text-gray-900 space-y-4 grid grid-cols-2">
+                <form method="post" action="{{ route('enregistrementProduit',['id' => $produit->id_produit]) }}" id="panierGrid" class="p-6 text-gray-900 space-y-4 grid grid-cols-2">
+                    
+                
                     @foreach($produits as $produit)
                         <div>
                             <p>
                                 {{ $produit['produit']->produit }}
                             </p>
                         </div>
+
                         <div>
                             <p>
-                                {{ $produit['quantite']}}
+                                {{ $produit['produit']->produit }}
+                            </p>
+                        </div>
+
+                        <div>
+                            <p>
+                                {{ $produit['produit']->produit }}
+                            </p>
+                        </div>
+
+                        <div>
+                            <p>
+                                {{ $produit['produit']->produit }}
                             </p>
                         </div>
 
 
                     @endforeach
 
-                </div>
+                </form>
             </div>
         </div>
     </div>
